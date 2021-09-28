dd9ab3dfaba89305c85af616d461c53c2ef06857
Michael Wong <michaelwong1391@gmail.com>
2021-09-28 13:43:45 -0400

    most likely finished myfilter method with test cases

--- assignment2.js
+++ assignment2.js
@@ -16,7 +16,9 @@ Array.prototype.myEach = function( callbackFn) {
 arr.forEach(l => console.log(l));
 arr.myEach(l => console.log(l));
 arr.forEach(l => console.log(l * 2));
-arr.myEach(l => console.log(l * 2));*/
+arr.myEach(l => console.log(l * 2));
+console.log(arr.myEach(()=>{}));
+console.log(arr.forEach(()=>{}));*/
 
 
 // MAP //
@@ -41,7 +43,7 @@ console.log(arr.map(x=>x * 2));
 console.log(arr.myMap(x=>x * 2));*/
 
 // FILTER //
-Array.prototype.myFilter = function() {
+Array.prototype.myFilter = function(callbackFn) {
    let new_arr = [];
    for (let i = 0; i < this.length; i++)
    {
@@ -50,10 +52,19 @@ Array.prototype.myFilter = function() {
       {
          continue;
       }*/
-      new_arr.push(callbackFn(this[i], i, this));
+      if (callbackFn(this[i], i, this))
+      {
+         new_arr.push(this[i]);
+      }
    }
    return new_arr;
 };
+
+// Testing
+
+/*let arr = [-1, 1, 7, "string", undefined, null];
+console.log(arr.filter(x=>x < 2));
+console.log(arr.myFilter(x=>x < 2));*/
 
 // SOME //
 Array.prototype.mySome = function(callbackFn) {
